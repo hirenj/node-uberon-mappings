ff4def51e864619b374559b1a58fa42574397bc2
Hiren Joshi <hirenj@gmail.com>
2017-03-29 23:19:18 +0200

    Improved brenda conversion

--- js/convert.js
+++ js/convert.js
@@ -7,28 +7,33 @@ const path = require('path');
 
 const new_mapping = () => { return {'p' : [], 'c' : [] }};
 
-const read_mappings = new Promise( (resolve,reject) => {
-  let instream = fs.createReadStream(path.join(__dirname,'../basic.obo'));
-  let mappings = {};
-  instream.pipe(obo.parse(() => {})).on('data',function(buffer) {
-    let dat = JSON.parse(buffer);
-    if ( ! mappings[dat.id] ){
-      mappings[dat.id] = new_mapping();
-    }
-    mappings[dat.id].value = dat;
-    handle_entry(dat,mappings[dat.id],mappings);
+const read_obo = function(file,handler) {
+  return new Promise( (resolve,reject) => {
+    let instream = fs.createReadStream(file);
+    let mappings = {};
+    instream.pipe(obo.parse(() => {})).on('data',function(buffer) {
+      let dat = JSON.parse(buffer);
+      if ( ! mappings[dat.id] ){
+        mappings[dat.id] = new_mapping();
+      }
+      mappings[dat.id].value = dat;
+      handler(dat,mappings[dat.id],mappings);
+    });
+    instream.on('end', () => {
+      resolve(mappings);
+    });
+    instream.on('error', err => reject(err) );
   });
-  instream.on('end', () => {
-    resolve(mappings);
-  });
-  instream.on('error', err => reject(err) );
-});
+};
 
-const handle_entry = function(entry,self,results) {
+const handle_entry_uberon = function(entry,self,results) {
   if (entry.intersection_of) {
     entry.relationship = (entry.relationship || []).concat(entry.intersection_of);
   }
-  [].concat(entry.is_a).filter( is_a => is_a ).map( is_a => is_a.split(' ')[0] ).forEach( is_a => {
+  [].concat(entry.is_a)
+  .filter( is_a => is_a )
+  .map( is_a => is_a.split(' ')[0] )
+  .forEach( is_a => {
     if (! results[is_a] ) {
       results[is_a] = new_mapping();
     }
@@ -46,6 +51,37 @@ const handle_entry = function(entry,self,results) {
     results[entry.id]['p'].push(relation);
   });
 };
+
+const handle_entry_brenda = function(entry,self,results) {
+  if (entry.intersection_of) {
+    entry.relationship = (entry.relationship || []).concat(entry.intersection_of);
+  }
+  [].concat(entry.is_a)
+  .filter( is_a => is_a )
+  .map( is_a => is_a.split(' ')[0] )
+  .forEach( is_a => {
+    if (! results[is_a] ) {
+      results[is_a] = new_mapping();
+    }
+    results[is_a]['c'].push(entry.id);
+    results[entry.id]['p'].push(is_a);
+  });
+  [].concat(entry.relationship)
+  .filter( relationship => relationship )
+  .filter( relationship => relationship.indexOf('part_of') >= 0 || relationship.indexOf('develops_from') >= 0 )
+  .map( relation => relation.split(' ')[1] )
+  .forEach( relation => {
+    if (! results[relation] ) {
+      results[relation] = new_mapping();
+    }
+    results[relation]['c'].push(entry.id);
+    results[entry.id]['p'].push(relation);
+  });
+};
+
+const read_mappings = read_obo(path.join(__dirname,'../basic.obo'),handle_entry_uberon);
+
+const read_obo_brenda = read_mappings.then( () => read_obo(path.join(__dirname,'../brenda.obo'),handle_entry_brenda));
 
 const read_roots = new Promise( (resolve,reject) => {
   let parser = csv({delimiter: "\t"}, function(err, data){
@@ -65,10 +101,10 @@ const read_brenda_mappings = new Promise( (resolve,reject) => {
       return;
     }
     let mappings = {};
-    data.forEach( row => mappings[row[4].toUpperCase()] = row[0] );
+    data.forEach( row => mappings[row[1].toUpperCase()] = row[0] );
     resolve(mappings);
   });
-  fs.createReadStream(path.join(__dirname,'../hetio-slim.tsv')).pipe(parser);
+  fs.createReadStream(path.join(__dirname,'../uberon_bto_mapping.tsv')).pipe(parser);
 });
 
 const parents_for = function(start,dist,mappings) {
@@ -116,18 +152,26 @@ const find_parent_term = function(term,roots,mappings) {
     return { term: term, root: null };
 };
 
+
 const convert_brenda = function(term) {
-  return read_brenda_mappings.then( mappings => {
-    return mappings[term.toUpperCase()];
-  }).then( converted => {
-    return { term: converted, original: term, toString: () => converted }
+  return Promise.all([read_obo_brenda,read_brenda_mappings]).then( data => {
+    let ontology = data[0];
+    let table = data[1];
+    if (table[term]) {
+      return table[term];
+    }
+    let mapped = find_parent_term(term,Object.keys(table),ontology);
+    return table[mapped.root];
+  })
+  .then( converted => {
+    return { term: converted, original: term, toString: () => converted };
   });
 }
 
 const convert = function(term) {
   let term_promise = Promise.resolve(term);
   if (term.toUpperCase().indexOf('BTO:') == 0) {
-    term_promise = convert_brenda(term);
+    term_promise = convert_brenda(term.toUpperCase());
   }
   return term_promise
   .then( (term) => Promise.all([term,read_mappings,read_roots]) )
